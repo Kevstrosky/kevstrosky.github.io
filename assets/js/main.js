db670edc703826ad16870f0a99d9202839fd2f28
Kevstrosky <kevstrosky>
2022-06-26 00:06:34 -0700

    for loop implemented in the idiom button

--- assets/js/main.js
+++ assets/js/main.js
@@ -71,62 +71,47 @@ sr.reveal(`.filters`, {delay: 1100})
 
 const idiomButton = document.getElementById('idiom-button');
 
-
-
-
 const english_information = {
-    //Header
-    profession: "Information Systems Engineering student",
-    //Download button
-    download: "Download CV <i class="+"ri-download-line"+"></i>", 
-    //Filter tabs
-    projects:  "Projects",
-    skills: "Skills",
-    experience: "Experience",
-    certifications: "Certifications",
-    //Skills title
-    skill_1_title: "Frontend development",
-    skill_2_title: "Backend development",
-    //Skills frontend
-    skill_1_level: "Intermediate", //HTML
-    skill_2_level: "Beginner", //Javascript
-    skill_3_level: "First steps", //Tailwindcss
-    skill_4_level: "Intermediate", //CSS
-    skill_5_level: "First steps", //Bootstrap
-    //Skills backend
-    skill_6_level: "Intermediate", //MySQL
-    skill_7_level: "Beginner", //Python
-    //Footer
-    footer: "Site based on the original design of Bedimcode. Edited and modified by Kevstrosky"
-
-
+    1: "Kevin Ochoa - Portfolio",
+    2: "Information Systems Engineering student",
+    3: "Download CV <i class="+"ri-download-line"+"></i>", 
+    4: "Projects",
+    5: "Skills",
+    6: "Experience",
+    7: "Certifications",
+    8: "Frontend development",
+    9: "Backend development",
+    10: "Intermediate", 
+    11: "Beginner", 
+    12: "First steps", 
+    13: "Intermediate", 
+    14: "First steps", 
+    15: "Intermediate", 
+    16: "Beginner", 
+    17: "Site based on the original design of Bedimcode. Edited and modified by Kevstrosky"
 }
+
 const spanish_information = {
-    //Header
-    profession: "Estudiante de Ingeniería en Sistemas de Información",
-    //Download button
-    download: "Descargar CV <i class="+"ri-download-line"+"></i>", 
-    //Filter tabs
-    projects:  "Proyectos",
-    skills: "Tecnologías",
-    experience: "Experiencia",
-    certifications: "Certificaciones",
-     //Skills title
-    skill_1_title: "Desarrollo frontend",
-    skill_2_title: "Desarrollo backend",
-    //Skills frontend
-    skill_1_level: "Intermediario", //HTML
-    skill_2_level: "Principiante", //Javascript
-    skill_3_level: "Primeros pasos", //Tailwindcss
-    skill_4_level: "Intermediario", //CSS
-    skill_5_level: "Primeros pasos", //Bootstrap
-    //Skills backend
-    skill_6_level: "Intermediario", //MySQL
-    skill_7_level: "Principiante", //Python
-    //Footer
-    footer: "Sitio basado en el diseño original de Bedimcode. Editado y modificado por Kevstrosky."
+    1: "Kevin Ochoa - Portafolio",
+    2: "Estudiante de Ingeniería en Sistemas de Información",
+    3: "Descargar CV <i class="+"ri-download-line"+"></i>", 
+    4: "Proyectos",
+    5: "Tecnologías",
+    6: "Experiencia",
+    7: "Certificaciones",
+    8: "Desarrollo frontend",
+    9: "Desarrollo backend",
+    10: "Intermediario", 
+    11: "Principiante", 
+    12: "Primeros pasos", 
+    13: "Intermediario", 
+    14: "Primeros pasos",
+    15: "Intermediario", 
+    16: "Principiante", 
+    17: "Sitio basado en el diseño original de Bedimcode. Editado y modificado por Kevstrosky."
 
 }
+
 
 idiomButton.addEventListener('click',() => {
     const list = document.querySelector(".change-idiom");
@@ -134,54 +119,20 @@ const style = getComputedStyle(list);
 const color = style.color;
 console.log(color);
     if(color == "rgb(34, 39, 43)" || color == "rgb(241, 242, 244)"){
-        document.getElementById('skill_1_title').innerHTML = english_information.skill_1_title;
-        document.getElementById('skill_2_title').innerHTML = english_information.skill_2_title;
-        document.getElementById('skill_1_level').innerHTML = english_information.skill_1_level;
-        document.getElementById('skill_2_level').innerHTML = english_information.skill_2_level;
-        document.getElementById('skill_3_level').innerHTML = english_information.skill_3_level;
-        document.getElementById('skill_4_level').innerHTML = english_information.skill_4_level;
-        document.getElementById('skill_5_level').innerHTML = english_information.skill_5_level;
-        document.getElementById('skill_6_level').innerHTML = english_information.skill_6_level;
-        document.getElementById('skill_7_level').innerHTML = english_information.skill_7_level;
-        document.getElementById('footer').innerHTML = english_information.footer;
         document.querySelector(".change-idiom").style.color = "var(--first-color)";
-        document.getElementById('profession').innerHTML = english_information.profession;
-        document.getElementById('cv-button').innerHTML = english_information.download;
-        document.getElementById('projects-button').innerHTML = english_information.projects;
-        document.getElementById('skills-button').innerHTML = english_information.skills;
-        document.getElementById('experience-button').innerHTML = english_information.experience;
-        document.getElementById('certifications-button').innerHTML = english_information.certifications;
 
-
-        /*
-        for(let i = 1; i <= 1; i++){
-            const {skill_1_level} = english_information;
-            document.getElementById('skill_'+i+'_level').innerHTML = english_information;
-        */
+        for(let i = 1; i <= Object.keys(english_information).length; i++){
+            document.getElementById(i).innerHTML = english_information[i];
+        }
 
     }
 
     if(color == "rgb(66, 165, 245)"){
         document.querySelector(".change-idiom").style.color = "var(--title-color)";
-        document.getElementById('skill_1_title').innerHTML = spanish_information.skill_1_title;
-        document.getElementById('skill_2_title').innerHTML = spanish_information.skill_2_title;
 
-        document.getElementById('skill_1_level').innerHTML = spanish_information.skill_1_level;
-        document.getElementById('skill_2_level').innerHTML = spanish_information.skill_2_level;
-        document.getElementById('skill_3_level').innerHTML = spanish_information.skill_3_level;
-        document.getElementById('skill_4_level').innerHTML = spanish_information.skill_4_level;
-        document.getElementById('skill_5_level').innerHTML = spanish_information.skill_5_level;
-        document.getElementById('skill_6_level').innerHTML = spanish_information.skill_6_level;
-        document.getElementById('skill_7_level').innerHTML = spanish_information.skill_7_level;
-
-        document.getElementById('footer').innerHTML = spanish_information.footer;
-        document.getElementById('profession').innerHTML = spanish_information.profession;
-        document.getElementById('cv-button').innerHTML = spanish_information.download;
-        document.getElementById('projects-button').innerHTML = spanish_information.projects;
-        document.getElementById('skills-button').innerHTML = spanish_information.skills;
-        document.getElementById('experience-button').innerHTML = spanish_information.experience;
-        document.getElementById('certifications-button').innerHTML = spanish_information.certifications;
-
+        for(let i = 1; i <= Object.keys(spanish_information).length; i++){
+            document.getElementById(i).innerHTML = spanish_information[i];
+        }
     }
 
 });
